3467048d4412b12a6b1d668594597b9ac2852094
Piotr Nowinski <pitred007git@gmail.com>
2019-02-24 18:25:39 +0100

    Init project

---
+++ js/script.js
@@ -0,0 +1,56 @@
+var CtoFBtn = document.getElementById('CtoF__button');
+var FtoCBtn = document.getElementById('FtoC__button');
+var output = document.getElementById('output__div');
+var output2 = document.getElementById('output__info');
+var state;
+var tempC;
+var tempF;
+
+output.innerHTML = '<br> Click the button to convert temperature' + '<br><br>';
+
+CtoF__button.addEventListener('click', function () {
+    var tempC = window.prompt("Enter temperature in C");
+    if (tempC === "" || tempC === null) {
+        output.innerHTML = ('');
+        output2.innerHTML = ('');
+    } else if (isNaN(tempC)) {
+        output.innerHTML = ('<br> Incorrect value');
+        output2.innerHTML = ('');
+    } else if (tempC < 0) {
+        state = 'In this temperature water is changing to ICE';
+    } else if (tempC >= 0 && tempC < 100) {
+        state = 'In this temperature water is LIQUID';
+    } else {
+        state = 'In this temperature water is changing to STEAM';
+    }
+
+    tempC = parseInt(tempC);
+    if (!isNaN(tempC)) {
+        var tempF = Math.round(tempC * 9 / 5 + 32);
+        output.innerHTML = '<br> It is ' + tempF + ' &#176;' + 'F <br><br>';
+        output2.innerHTML = '<br>' + state + '<br>';
+    }
+});
+
+FtoC__button.addEventListener('click', function () {
+    var tempF = window.prompt('Enter temperature in F');
+    if (tempF === "" || tempF === isNaN || tempF === null) {
+        output.innerHTML = ('');
+        output2.innerHTML = ('');
+    } else if (isNaN(tempF)) {
+        output.innerHTML = ('<br> Incorrect value');
+        output2.innerHTML = ('');
+    } else if (tempF < 32) {
+        state = 'In this temperature water is changing to ICE';
+    } else if (tempF >= 32 && tempF < 212) {
+        state = 'In this temperature water is LIQUID';
+    } else {
+        state = 'In this temperature water is changing to STEAM';
+    }
+    tempF = parseInt(tempF);
+    if (!isNaN(tempF)) {
+        var tempC = Math.round((tempF - 32) * 5 / 9);
+        output.innerHTML = '<br> It is ' + tempC + ' &#176;' + 'C <br><br>';
+        output2.innerHTML = '<br>' + state + '<br>';
+    }
+});
